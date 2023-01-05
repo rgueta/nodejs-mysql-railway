f1a40f2323060d2fa1adb5357758f2a971339aa2
Gueta <ricardogueta@hotmail.com>
2023-01-05 11:59:00 -0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { pool } from "./db.js";
+import { PORT } from "./config.js";
+
+const app = express()
+
+app.get('/', async (req,res)=>{
+    const [result] = await pool.query(`SELECT * FROM users`)
+    console.log(result)
+    res.json(result)
+})
+
+app.get('/ping',async (req,res) => {
+    const result = await pool.query(`SELECT "Hello world" as RESULT`)
+    console.log(result[0])
+    res.json(result[0])
+})
+
+app.get('/create',async (req,res) => {
+    const result = await pool.query(`INSERT INTO users(name) VALUES("John")`)
+    console.log(result[0])
+    res.json(result[0])
+})
+
+app.listen(PORT)
+console.log('server on port', PORT)
---
+++ src/db.js
@@ -0,0 +1,11 @@
+import { createPool } from "mysql2/promise";
+import { DB_HOST, DB_NAME,DB_PORT, DB_USER, DB_PASSWORD, } from "./config.js";
+
+export const pool = createPool({
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME
+});
+
